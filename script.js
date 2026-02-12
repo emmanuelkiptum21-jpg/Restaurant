9e34f2aec248f55daf0ff3c59179be3943172a8a
Emmanuel <emmanuelkiptum21@gmail.com>
2026-02-12 15:35:08 +0300

    remove nav menu from js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 
-
-
-const menuOpenButton = document.getElementById("menu-open-button");
-const menuCloseButton = document.getElementById("menu-close-button");
-const navMenu = document.querySelector(".nav-menu");
-
-// Open mobile menu
-menuOpenButton.addEventListener("click", () => {
-    navMenu.style.left = "0"; // slide in
-});
-
-// Close mobile menu
-menuCloseButton.addEventListener("click", () => {
-    navMenu.style.left = "-300px"; // slide out
-});
 const orderButton = document.querySelector(".order-now");
 const contactButton = document.querySelector(".contact-us");
 
